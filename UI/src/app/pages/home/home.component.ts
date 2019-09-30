beb885a2001c2b139bda2d0ef6d1c0867ad84a4a
Adrian Rakovsky <adrian.rakovsky@gratex.com>
2019-09-30 16:24:49 +0200

    gauges

--- UI/src/app/pages/home/home.component.ts
+++ UI/src/app/pages/home/home.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ChartDataSets, ChartOptions } from 'chart.js';
-import { Label, Color, BaseChartDirective } from 'ng2-charts';
-import { ReadingsService } from '../../services/readings.service';
-import { shareReplay, map, filter, distinctUntilChanged, debounce, expand, delay, take, debounceTime, switchMap } from 'rxjs/operators';
-import { Observable, ReplaySubject, interval, of, empty } from 'rxjs';
-import { groupBy, sumBy, orderBy } from 'lodash-es';
+import { Label } from 'ng2-charts';
+import { ReadingsService, IStats } from '../../services/readings.service';
+import { shareReplay, map, filter, distinctUntilChanged, take, switchMap } from 'rxjs/operators';
+import { Observable, interval, combineLatest } from 'rxjs';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 import * as pluginAnnotation from 'chartjs-plugin-annotation';
 
-const defaultBarColor = 'rgba(23, 162, 184, 0.7)';
-const currentYearBarColor = 'rgba(0, 118, 138, 0.7)';
-const currentYear = new Date().getFullYear();
+const DEFAULT_BAR_COLOR = 'rgba(23, 162, 184, 0.7)';
+const CURRENT_YEAR_BAR_COLOR = 'rgba(0, 118, 138, 0.7)';
+const CURRENT_YEAR = new Date().getFullYear();
+const PAGES_PER_DAY = 30;
 
 @Component({
   selector: 'app-home',
@@ -20,88 +20,173 @@ const currentYear = new Date().getFullYear();
 export class HomeComponent implements OnInit {
 
   public isLoading = true;
-  public barChartLabels1$: Observable<Label[]>;
-  public barChartLabels2$: Observable<Label[]>;
+  public booksByYearLabels$: Observable<Label[]>;
+  public pagesByYearLabels$: Observable<Label[]>;
 
-  public barChartData1$: Observable<ChartDataSets[]>;
-  public barChartData2$: Observable<ChartDataSets[]>;
+  public booksByYearData$: Observable<ChartDataSets[]>;
+  public pagesByYearData$: Observable<ChartDataSets[]>;
+
+  public booksByYearOpts: ChartOptions;
+  public pagesByYearOpts: ChartOptions;
 
   public plugins = [pluginDataLabels, pluginAnnotation];
-  public size$: Observable<number>;
 
-  public isLeapYear = new Date(new Date().getFullYear(), 1, 29).getDate() === 29;
-  public dayOfYear = dayOfYear();
+  public gauges$: Observable<Array<{ name: string, label: string, opts: any; size: number }>>
 
-  public chartOpts1: ChartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      datalabels: { anchor: 'end', color: 'white', offset: 0, align: 'left' },
-    },
-    scales: { xAxes: [{ ticks: { beginAtZero: true, suggestedMax: 50 } }] },
-  }
-
-  public chartOpts2: ChartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      datalabels: { anchor: 'end', color: 'white', offset: 0, align: 'left' },
-    },
-    scales: { xAxes: [{ ticks: { beginAtZero: true } }] },
-  }
-
-  @ViewChild('gauge1', { static: false }) gauge1: ElementRef;
+  @ViewChild('gauge', { static: false }) gauge: ElementRef;
 
   ngAfterViewInit() { }
 
-  constructor(private readingService: ReadingsService) { }
+  constructor(public readingService: ReadingsService) {
+    this.booksByYearOpts = this.getChartOptions();
+    this.pagesByYearOpts = this.getChartOptions(50);
+  }
 
   ngOnInit() {
-    // todo extract to service - all this countings
-    const data$ = this.readingService.getStats().pipe(shareReplay());
+    const stats$ = this.readingService.getStats().pipe(shareReplay());
 
-    const barChartData1All$ = data$.pipe(map((data) => {
-      return orderBy(Object.entries(groupBy(data, 'year')).map(([year, readings]) => [year, (readings as any).length]), '1').reverse();
-    }));
-    this.barChartLabels1$ = barChartData1All$.pipe(map((data) => data.map(([year, pages]) => year)));
-    this.barChartData1$ = barChartData1All$.pipe(map(buildDataSets));
+    this.booksByYearLabels$ = stats$.pipe(map(({ booksByYear }) => booksByYear.map(([year], i) => `[${i + 1}.]   ${year}`)));
+    this.booksByYearData$ = stats$.pipe(map(({ booksByYear }) => this.buildChartDataSet(booksByYear)));
 
-    const barChartData2All$ = data$.pipe(map((data) => {
-      return orderBy(Object.entries(groupBy(data, 'year')).map(([year, readings]) => [year, sumBy(readings, 'pages')]), '1').reverse();
-    }));
-    this.barChartLabels2$ = barChartData2All$.pipe(map((data) => data.map(([year, pages]) => year)));
-    this.barChartData2$ = barChartData2All$.pipe(map(buildDataSets));
+    this.pagesByYearLabels$ = stats$.pipe(map(({ pagesByYear }) => pagesByYear.map(([year], i) => `[${i + 1}.]   ${year}`)));
+    this.pagesByYearData$ = stats$.pipe(map(({ pagesByYear }) => this.buildChartDataSet(pagesByYear)));
 
-    data$.subscribe({ next: () => this.isLoading = false });
+    stats$.subscribe({ next: () => this.isLoading = false });
 
-    this.size$ = interval(100).pipe(
-      filter(() => !!this.gauge1),
-      map(() => Math.floor(this.gauge1.nativeElement.getBoundingClientRect().width - 30)),
+
+    this.initGauges(stats$);
+
+  }
+  private initGauges(stats$: Observable<IStats>) {
+    const { dayOfYear, isLeapYear } = this.readingService
+    const size$ = this.getGaugeSize();
+
+    this.gauges$ = combineLatest(stats$, size$, ({ pagesCurrentYear }, size) => {
+
+      const perDayToToday = pagesCurrentYear / dayOfYear;
+      const perDayToTodayPerc = perDayToToday / 30;
+
+      const perDayToEndOfYear = pagesCurrentYear / (isLeapYear ? 366 : 365);
+      const perDayToEndOfYearPerc = perDayToEndOfYear / 30;
+
+      const planToToday = dayOfYear * PAGES_PER_DAY;
+      const planToTodayPerc = pagesCurrentYear / planToToday;
+
+      const planToEndOfYear = (isLeapYear ? 366 : 365) * PAGES_PER_DAY;
+      const planToEndOfYearPerc = pagesCurrentYear / planToEndOfYear;
+
+      return [
+        {
+          name: 'Strán / deň (dnes)',
+          label: decimal(perDayToToday),
+          size,
+          opts: {
+            arcColors: perDayToTodayPerc == 0 ? ['lightgray'] : [DEFAULT_BAR_COLOR, 'lightgray'],
+            arcDelimiters: perDayToTodayPerc >= 1 || perDayToTodayPerc == 0 ? [] : [perDayToTodayPerc * 100],
+            arcLabels: [perc(perDayToTodayPerc)],
+            arcLabelFontSize: 12,
+            rangeLabel: ['0', '30']
+          }
+        },
+        {
+          name: 'Strán / deň (rok)',
+          label: decimal(perDayToEndOfYear),
+          size,
+          opts: {
+            arcColors: perDayToEndOfYearPerc == 0 ? ['lightgray'] : [DEFAULT_BAR_COLOR, 'lightgray'],
+            arcDelimiters: perDayToEndOfYearPerc >= 1 || perDayToEndOfYearPerc == 0 ? [] : [perDayToEndOfYearPerc * 100],
+            arcLabels: [perc(perDayToEndOfYearPerc)],
+            arcLabelFontSize: 12,
+            rangeLabel: ['0', '30']
+          }
+        },
+        {
+          name: 'Strán (dnes)',
+          label: pagesCurrentYear.toLocaleString('sk'),
+          size,
+          opts: {
+            arcColors: planToTodayPerc == 0 ? ['lightgray'] : [DEFAULT_BAR_COLOR, 'lightgray'],
+            arcDelimiters: planToTodayPerc >= 1 || planToTodayPerc == 0 ? [] : [planToTodayPerc * 100],
+            arcLabels: [perc(planToTodayPerc)],
+            arcLabelFontSize: 12,
+            rangeLabel: ['0', planToToday.toLocaleString('sk')]
+          }
+        },
+        {
+          name: 'Strán (rok)',
+          label: pagesCurrentYear.toLocaleString('sk'),
+          size,
+          opts: {
+            arcColors: planToEndOfYearPerc == 0 ? ['lightgray'] : [DEFAULT_BAR_COLOR, 'lightgray'],
+            arcDelimiters: planToEndOfYearPerc >= 1 || planToEndOfYearPerc == 0 ? [] : [planToEndOfYearPerc * 100],
+            arcLabels: [perc(planToEndOfYearPerc)],
+            arcLabelFontSize: 12,
+            rangeLabel: ['0', planToEndOfYear.toLocaleString('sk')]
+          }
+        }/*,
+        {
+          name: 'Deň v roku',
+          label: `${dayOfYear}`,
+          size,
+          opts: {
+            arcColors: [gaugeDefaultColor, 'lightgray'],
+            arcLabels: [perc(dayOfYear / (isLeapYear ? 366 : 365))],
+            arcLabelFontSize: 12,
+            arcDelimiters: [Math.round(dayOfYear / (isLeapYear ? 366 : 365) * 100)],
+            rangeLabel: ['1', isLeapYear ? '366' : '365']
+          }
+        }*/
+      ];
+
+      function perc(val) {
+        return val.toLocaleString('sk', { style: 'percent', minimumFractionDigits: 1, maximumFractionDigits: 1 });
+      }
+
+      function decimal(val) {
+        return val.toLocaleString('sk', { minimumFractionDigits: 1, maximumFractionDigits: 1 });
+      }
+    });
+    /*
+  plnenie planu 30*365
+  plnene planu 30*365/den
+  plan k aktualnemu dnu a ku koncu roka
+  plus-minus dni
+  plus-minus stran
+  
+    */
+  }
+
+  private getGaugeSize(): Observable<number> {
+    return interval(100).pipe(
+      filter(() => !!this.gauge),
+      map(() => Math.floor(this.gauge.nativeElement.getBoundingClientRect().width - 30)),
       distinctUntilChanged(),
       switchMap((val) => {
         const arr = [0, val];
         return interval(0).pipe(map(() => arr.shift()), take(2));
       })
     );
+  }
 
-    function buildDataSets(data: any[]): ChartDataSets[] {
-      return [{
-        data: data.map(([year, pages]) => pages),
-        backgroundColor: data.map(([year]) => year == currentYear ? currentYearBarColor : defaultBarColor),
-        hoverBackgroundColor: data.map(([year]) => (year == currentYear ? currentYearBarColor : defaultBarColor).replace('0.7', '0.8')),
-        hoverBorderColor: 'transparent'
-      }];
+  private buildChartDataSet(data: [number, number][]): ChartDataSets[] {
+    return [{
+      data: data.map(([year, value]) => value),
+      backgroundColor: data.map(([year]) => year == CURRENT_YEAR ? CURRENT_YEAR_BAR_COLOR : DEFAULT_BAR_COLOR),
+      hoverBackgroundColor: data.map(([year]) => (year == CURRENT_YEAR ? CURRENT_YEAR_BAR_COLOR : DEFAULT_BAR_COLOR).replace('0.7', '0.8')),
+      hoverBorderColor: 'transparent'
+    }];
+  }
+
+  private getChartOptions(suggestedMax?: number): ChartOptions {
+    return {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        datalabels: { anchor: 'end', color: 'white', offset: 0, align: 'left' },
+      },
+      scales: { xAxes: [{ ticks: { beginAtZero: true, suggestedMax } }] },
     }
   }
 
 
-}
-
-
-function dayOfYear() {
-  var now = new Date();
-  var start = new Date(now.getFullYear(), 0, 0);
-  var diff = (now as any) - (start as any);
-  var oneDay = 1000 * 60 * 60 * 24;
-  return Math.floor(diff / oneDay);
 }
--- UI/src/app/services/readings.service.ts
+++ UI/src/app/services/readings.service.ts
@@ -1,21 +1,66 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
+import { shareReplay, map } from 'rxjs/operators';
+import { groupBy, sumBy, orderBy } from 'lodash-es';
+
+export interface IStats {
+  booksByYear: Array<[number, number]>;
+  pagesByYear: Array<[number, number]>;
+  booksCurrentYear: number;
+  pagesCurrentYear: number;
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReadingsService {
 
-  constructor(private http: HttpClient) { }
+  private stats$: Observable<any>;
+  public isLeapYear: boolean;
+  public dayOfYear: number;
+  public year: number;
+
+  constructor(private http: HttpClient) {
+    this.isLeapYear = new Date(new Date().getFullYear(), 1, 29).getDate() === 29;
+    this.dayOfYear = dayOfYear();
+    this.year = new Date().getFullYear();
+  }
 
   public listReadings() {
     return this.http.get(`${environment.baseUrl}/svc/reading/list/`);
   }
 
-  public getStats() {
-    return this.http.get(`${environment.baseUrl}/svc/reading/stats/`);
+  private getStatsData() {
+    if (!this.stats$) {
+      this.stats$ = this.http.get(`${environment.baseUrl}/svc/reading/stats/`).pipe(shareReplay());
+    }
+    return this.stats$;
   }
 
+  public getStats(): Observable<IStats> {
+    const booksByYear$ = this.getStatsData().pipe(map((data) => {
+      return orderBy(Object.entries(groupBy(data, 'year')).map(([year, readings]) => [+year, (readings as any).length]), '1').reverse();
+    }));
+    const pagesByYear$ = this.getStatsData().pipe(map((data) => {
+      return orderBy(Object.entries(groupBy(data, 'year')).map(([year, readings]) => [+year, sumBy(readings, 'pages')]), '1').reverse();
+    }));
+
+    return combineLatest<IStats>(booksByYear$, pagesByYear$, (booksByYear, pagesByYear) => ({
+      booksByYear,
+      pagesByYear,
+      booksCurrentYear: (booksByYear.find(([year]) => year == this.year) || [0, 0])[1],
+      pagesCurrentYear: (pagesByYear.find(([year]) => year == this.year) || [0, 0])[1],
+      pagesYearTarget: 30 * (this.isLeapYear ? 366 : 366)
+    }));
+  }
+}
+
+function dayOfYear() {
+  var now = new Date();
+  var start = new Date(now.getFullYear(), 0, 0);
+  var diff = (now as any) - (start as any);
+  var oneDay = 1000 * 60 * 60 * 24;
+  return Math.floor(diff / oneDay);
 }
