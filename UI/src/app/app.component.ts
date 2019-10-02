5b212556aa2a89b120da38434d38757fb7d10212
Adrian Rakovsky <adrian.rakovsky@gratex.com>
2019-10-02 17:26:35 +0200

    hiding search

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Renderer2, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { environment } from '../environments/environment';
 import { Router } from '@angular/router';
 import { Observable, of, timer } from 'rxjs';
@@ -10,7 +10,7 @@ import { SearchService } from './services/search.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   version = environment.releaseVersion;
   versionTitle = `${environment.releaseVersion} ${environment.releaseCreatedAt} ${environment.slugCommit}`;
 
@@ -19,10 +19,18 @@ export class AppComponent {
   public searchInputChange$ = new EventEmitter<Event>();
   public searchResult$: Observable<any>;
 
-  constructor(private router: Router, private searchService: SearchService) {
+  @ViewChild('searchInput', { static: true })
+  private searchInput: ElementRef;
+
+  @ViewChild('dropdown', { static: false })
+  private dropdown: ElementRef;
+
+  private documentClickListener: () => void;
+
+  constructor(private router: Router, private searchService: SearchService, private renderer: Renderer2) {
     router.events.subscribe(() => {
       this.navbarOpen = false;
-      this.searchInputChange$.next({ target: { value: null } } as any);
+      this.hideSearchResult();
     });
 
     this.searchResult$ = this.searchInputChange$.pipe(
@@ -30,9 +38,26 @@ export class AppComponent {
       debounce((val) => val ? timer(200) : of(0)),
       switchMap((value) => value ? this.searchService.search(value).pipe(startWith('LOADING')) : of(null))
     );
+
+    this.documentClickListener = this.renderer.listen('document', 'click', (event) => {
+      if (event.button !== 2
+        && !this.searchInput.nativeElement.contains(event.target)
+        && (!this.dropdown || !this.dropdown.nativeElement.contains(event.target))
+      ) {
+        this.hideSearchResult()
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.documentClickListener();
   }
 
   public toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
+  }
+
+  private hideSearchResult() {
+    this.searchInputChange$.next({ target: { value: null } } as any);
   }
 }
